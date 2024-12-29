28cb3a695bb32f3892e71106524ef94cedbab95c
Lucas Sabino <82790812+LucasSabino912@users.noreply.github.com>
2024-12-29 20:00:12 -0300

    find-closest-number-to-zero.c updated

--- C/find-closest-number-to-zero.c
+++ C/find-closest-number-to-zero.c
@@ -10,27 +10,16 @@
 #include <stdio.h>
 #include <math.h>
 
-// Define my own absolute function
-int myAbs(int n){
-    return n < 0 ? -n : n;
-}
-
 int findClosestNumber(int* nums, int numsSize) {
     int closest = nums[0];
     for (int i = 0; i < numsSize; i++){
-        // Immediate return 0 if we get 0 as number in the array
-        if(nums[i] == 0){
-            return 0;
-
-        }
-        // Check for the absolute minimum element 
-        if(myAbs(nums[i]) < myAbs(closest)){
+        if((abs(nums[i]) < abs(closest)) || ((abs(closest) == abs(nums[i]) && (nums[i] >= closest)))){
             closest = nums[i];
-        }
-        // If we have the same difference, always consider the positive 
-        else if ((myAbs(nums[i]) == myAbs(closest)) && nums[i] > closest){
-            closest = nums[i];
-        }
+        }  
     }
     return closest;
+}
+
+int abs(int n){
+    return (n < 0) ? -n : n;
 }
