177c9bcbb3a9976aadf21e36654045018da06cf7
Lucas Sabino <82790812+LucasSabino912@users.noreply.github.com>
2025-01-02 13:47:05 -0300

    Update is-subsequence.c

--- C/is-subsequence.c
+++ C/is-subsequence.c
@@ -5,24 +5,21 @@
 
     Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
-    A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the         characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+    A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the 
+    characters without disturbing the relative positions of the remaining characters.
 */
 
-#include <stdbool.h>
-#include <string.h>
-
 bool isSubsequence(char* s, char* t) {
-    int len1 = strlen(s);
-    int len2 = strlen(t);
-
+    int lens = strlen(s);
+    int lent = strlen(t);
     int i = 0, j = 0;
 
-    while(i < len1 && j < len2){
+    while(i < lens && j < lent){
         if(s[i] == t[j]){
-            i++; // Increments only if they are equal
+            i++;
         }
         j++;
     }
 
-    return len1 == i;
+    return i == lens;
 }
