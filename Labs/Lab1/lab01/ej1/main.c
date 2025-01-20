aefb8cf9db54ad690437a82d540bcb0a4ba16082
Lucas <lucas.sabino@mi.unc.edu.ar>
2025-01-20 12:41:55 -0300

    Ejercicio 1: Array_from_file() y Array_dump()

--- Labs/Lab1/lab01/ej1/main.c
+++ Labs/Lab1/lab01/ej1/main.c
@@ -41,14 +41,49 @@ char *parse_filepath(int argc, char *argv[]) {
     return result;
 }
 
+// Esta funcion devuelve el tamaño del arreglo leido en el archivo
 unsigned int array_from_file(int array[],
                              unsigned int max_size,
                              const char *filepath) {
-    //your code here!!!
+    FILE *file = fopen(filepath, "r"); // Abro el archivo en modo lectura
+
+    // Chequeo que se haya abierto de forma correcta
+    if(file == NULL){
+        exit(EXIT_FAILURE);
+    }
+
+    // Ahora obtengo los datos del array
+    // Primero obtengo el tamaño del array
+    unsigned int size;
+    fscanf(file, "%u", &size);
+
+    // Chequeo que el tamaño supere el tamaño maximo
+    if(size > max_size){
+        exit(EXIT_FAILURE);
+    }
+
+    // Se leen 'size' caracteres del archivo
+    // Se almacenan en ls posiciones '0' hasta 'size - 1'
+    for (unsigned int i = 0; i < size; i++){
+        fscanf(file, "%d", &array[i]);
+    }
+    
+    // Cierro el archivo y devuelvo el tamaño
+    fclose(file);
+    return size;
+     
 }
 
+// Esta funcion imprime los datos del input como  un arreglo
 void array_dump(int a[], unsigned int length) {
-    //your code here!!!
+    
+    printf("[%d", a[0]);
+
+    for (unsigned int i = 1u; i < length; i++) {
+        printf(", %d", a[i]);
+    }
+
+    printf("]\n");
 }
 
 
