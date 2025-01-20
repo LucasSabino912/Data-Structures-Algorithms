26f1c0a4e576bc64dd645b2c5ac97984675f63ad
Lucas <lucas.sabino@mi.unc.edu.ar>
2025-01-20 17:50:03 -0300

    Ejercicio 0: manejo de strings

--- Labs/Lab2/lab02/ej0/fixstring.c
+++ Labs/Lab2/lab02/ej0/fixstring.c
@@ -4,23 +4,51 @@
 #include "fixstring.h"
 
 unsigned int fstring_length(fixstring s) {
-    /*
-     * COMPLETAR
-     *
-     */
+    unsigned int i = 0u;
+    while (s[i] != '\0'){
+        i++;
+    }
+
+    return i; 
 }
 
 bool fstring_eq(fixstring s1, fixstring s2) {
-    /*
-     * COMPLETAR
-     *
-     */
+    unsigned int i = 0;
+    unsigned int lengths1 = 0, lengths2;
+
+    lengths1 = fstring_length(s1);
+    lengths2 = fstring_length(s2);
+
+    if (lengths1 != lengths2){
+        return false;
+    }
+
+    while(s1[i] != '\0' && s2[i] != '\0'){
+        if(s1[i] == s2[i]){
+            i++;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 bool fstring_less_eq(fixstring s1, fixstring s2) {
-    /*
-     * COMPLETAR
-     *
-     */
+    unsigned int i = 0;
+
+    if(s1 == s2){
+        return false;
+    }
+
+    while (s1[i] != '\0' && s2[i] != '\0'){
+        if(s1[i] < s2[i]){
+            i++;
+        } else {
+            return false;
+        }
+    }
+    
+    return true;
 }
 
