70cc7d9ebeea8cc2032a3b3ea32784a454c15b40
Lucas <lucas.sabino@mi.unc.edu.ar>
2025-01-21 16:44:21 -0300

    Ejercicio 1: Insertion sort

--- Labs/Lab2/lab02/ej1/sort.c
+++ Labs/Lab2/lab02/ej1/sort.c
@@ -7,13 +7,23 @@
 #include "sort.h"
 
 
+/* Aplico el algoritmo insert de siempre del insertion sort,
+   con la diferencia que uso la funcion goes_before(), */
 static void insert(int a[], unsigned int i) {
-    /* needs implementation */
+    while (i > 0 && goes_before(a[i], a[i-1])){
+        swap(a, i-1, i);
+        i--;
+    }
+    
 }
+
+/* El insertion sort de siempre con la unica diferencia que chequeo en cada iteracion
+   si el algoritmo esta ordenado, esto hara que en el mejor de los casos el algoritmo
+   termine antes  */
 
 void insertion_sort(int a[], unsigned int length) {
     for (unsigned int i = 1u; i < length; ++i) {
-        /* needs implementation */
+        assert(array_is_sorted(a, length));
         insert(a, i);
     }
 }
