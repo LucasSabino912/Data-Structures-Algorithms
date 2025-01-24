e3b4c6f7a967694d9bb29d90beeb9cd51d6db251
Lucas <lucas.sabino@mi.unc.edu.ar>
2025-01-24 19:10:34 -0300

    Ejercicio 3: Puntero a estructura

--- Labs/04 - Punteros y TADs/lab04/ej3/sort.c
+++ Labs/04 - Punteros y TADs/lab04/ej3/sort.c
@@ -12,8 +12,7 @@
 #include "player.h"
 
 bool goes_before(player_t x, player_t y){
-    // completar aquí
-    return true;
+    return x->rank > y->rank;
 }
 
 bool array_is_sorted(player_t atp[], unsigned int length) {
@@ -24,7 +23,25 @@ bool array_is_sorted(player_t atp[], unsigned int length) {
     return (i == length);
 }
 
-void sort(player_t a[], unsigned int length) {
-    // completar aquí
+static void swap(player_t a[], unsigned int x, unsigned int y){
+    player_t tmp = a[x];
+    a[x] = a[y];
+    a[y] = tmp;
 }
 
+static void insert(player_t a[], unsigned int i) {
+    while (i > 0 && goes_before(a[i], a[i-1])){
+        swap(a, i-1, i);
+        i--;
+    }
+    
+}
+
+// Ordenar por puesto en el ranking usando el insertion sort
+void sort(player_t a[], unsigned int length) {
+    for (unsigned int i = 0; i < length; i++){
+        assert(array_is_sorted(a, length));
+        insert(a, i);
+    }
+    
+}
