871322a8f61adf3b24e5cbc5365eca87e1a14279
Lucas <lucas.sabino@mi.unc.edu.ar>
2025-01-24 19:39:27 -0300

    Ejercicio 4: Arreglos con punteros

--- Labs/04 - Punteros y TADs/lab04/ej4/c-sizes/sizes.c
+++ Labs/04 - Punteros y TADs/lab04/ej4/c-sizes/sizes.c
@@ -18,15 +18,33 @@ int main(void) {
     print_data(messi);
 
     printf("name-size  : %lu bytes\n"
-           "age-size   : %lu bytes\n"
-           "height-size: %lu bytes\n"
-           "data_t-size: %lu bytes\n", /* COMPLETAR */);
+            "age-size   : %lu bytes\n"
+            "height-size: %lu bytes\n"
+            "data_t-size: %lu bytes\n",
+            sizeof(messi.name),
+            sizeof(messi.age),
+            sizeof(messi.height),
+            sizeof(messi));
 
-    /*
-     *
-     * COMPLETAR
-     *
-     */
+
+    printf("name direction  : %p\n"
+            "age direcion    : %p\n"
+            "height direcion : %p\n"
+            "data_t direction : %p\n",
+            (void *)&messi.name,
+            (void *)&messi.age,
+            (void *)&messi.height,
+            (void *)&messi);
+
+    printf("name-index   : %lu\n"
+            "age-index   : %lu\n"
+            "height-index: %lu\n"
+            "data_t-index: %lu\n",
+            (uintptr_t)&messi.name,
+            (uintptr_t)&messi.age,
+            (uintptr_t)&messi.height,
+            (uintptr_t)&messi);
+ 
     return EXIT_SUCCESS;
 }
 
