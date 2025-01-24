50b474a69f8a35ab13538508280ba11a518f647d
Lucas <lucas.sabino@mi.unc.edu.ar>
2025-01-24 11:09:56 -0300

    Ejercicio 2: carga de datos de arrays multidimensionales

--- Labs/03 - Matrices/lab03/ej2/weather.c
+++ Labs/03 - Matrices/lab03/ej2/weather.c
@@ -10,7 +10,15 @@ static const int AMOUNT_OF_WEATHER_VARS = 6 ;
 Weather weather_from_file(FILE* file)
 {
     Weather weather;
-    /* Completar aqui */
+    int res = fscanf(file, "%d %d %d %u %u %u", &weather._average_temp,
+                                                &weather._max_temp,
+                                                &weather._min_temp,
+                                                &weather._pressure,
+                                                &weather._moisture,
+                                                &weather._rainfall);
+    if(res != AMOUNT_OF_WEATHER_VARS){
+      fprintf(stderr, "amount o weather vars incorrect");
+    }
     return weather;
 }
 
@@ -19,3 +27,15 @@ void weather_to_file(FILE* file, Weather weather)
     fprintf(file, EXPECTED_WEATHER_FILE_FORMAT, weather._average_temp, 
             weather._max_temp, weather._min_temp, weather._pressure, weather._moisture, weather._rainfall);
 }
+
+/*
+typedef struct _weather
+{
+    int _average_temp;
+    int _max_temp;
+    int _min_temp;
+    unsigned int _pressure;
+    unsigned int _moisture;
+    unsigned int _rainfall;
+} Weather;
+*/
