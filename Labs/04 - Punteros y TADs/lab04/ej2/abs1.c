50e17a137f0429ec597580d1371ca9c7e0b951e0
Lucas <lucas.sabino@mi.unc.edu.ar>
2025-01-24 15:42:10 -0300

    Ejercicio 2: valor absoluto con punteros

--- Labs/04 - Punteros y TADs/lab04/ej2/abs1.c
+++ Labs/04 - Punteros y TADs/lab04/ej2/abs1.c
@@ -1,17 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* Simulamos el procedimiento absolute (proc = void) */
 void absolute(int x, int y) {
-    //
-    // Completar aquí
-    //
+    if (x >= 0){
+        y = x; 
+    } else {
+        y = -x;
+    }
 }
 
 int main(void) {
-    int a=0, res=0;
-    //
-    // Completar aquí
-    //
-    return EXIT_SUCCESS;
+    int a = 0, res = 0;
+    a = -10;
+    absolute(a, res);
+    printf("%d", res);
+    return EXIT_SUCCESS; // r:= 0
 }
 
+/*
+Siguiendo la defincion del teorico, el valor que se imprime es 10
+Pero en el programa, el valor es 0, ya que las variables x e y tienen el
+scope limitado a absolute
+*/
--- Labs/04 - Punteros y TADs/lab04/ej2/abs2.c
+++ Labs/04 - Punteros y TADs/lab04/ej2/abs2.c
@@ -2,20 +2,30 @@
 #include <stdio.h>
 #include <assert.h>
 
+// *y = valor de y, &y = direccion de memoria de y
 void absolute(int x, int *y) {
-    //
-    // Completar aquí
-    //
+    if (x >= 0){
+        *y = x;
+    } else {
+        *y = -x;
+    }
 }
 
 int main(void) {
-    int a=0, res=0;  // No modificar esta declaración
-    // --- No se deben declarar variables nuevas ---
+    int a = 0, res = 0;
+    a = -10;
 
-    //
-    // Completar aquí
-    //
+    absolute(a, &res); // res = 10
+
+    printf("%d", &res);
+
     assert(res >= 0 && (res == a || res == -a));
     return EXIT_SUCCESS;
 }
 
+/*
+*y en absolute nunca se modifica, solo se modifica el valor al que apunta
+
+En este programa, absolute puede modificar directyamente el valor de res 
+en main a traves de *y
+*/
