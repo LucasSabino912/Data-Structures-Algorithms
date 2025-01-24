a910c500dc3f1237393dd614c0d92956ecdf4fa6
Lucas <lucas.sabino@mi.unc.edu.ar>
2025-01-24 16:00:51 -0300

    Ejercicio 2: swap con punteros

---
+++ Labs/04 - Punteros y TADs/lab04/ej2/swap.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+    int tmp;
+    tmp = *x;  // Guarda el valor de *x en una variable temporal.
+    *x = *y;   // Asigna el valor de *y a *x.
+    *y = tmp;  // Asigna el valor guardado en tmp a *y.
+}
+
+int main(void){
+    int x = 6, y = 4;
+    printf("%d %d", x, y);
+
+    swap(&x, &y);
+
+    printf("%d %d", &x, &y);
+
+    return EXIT_SUCCESS;
+}
