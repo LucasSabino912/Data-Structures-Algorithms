c3694312bc6dfabbe376aa278c61893f03a5209b
Lucas <lucas.sabino@mi.unc.edu.ar>
2025-01-25 17:43:39 -0300

    Ejercicio 5: TAD par con punteros y polimorfismo

---
+++ Labs/04 - Punteros y TADs/lab04/ej5/pair_e-morf/pair.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>  /* EXIT_SUCCESS... */
+#include <stdio.h>   /* printf()...     */
+#include "pair.h"    /* TAD Par         */
+
+struct s_pair_t {
+    elem fst;
+    elem snd;
+};
+
+pair_t pair_new(elem x, elem y){
+    pair_t p = malloc(sizeof(struct s_pair_t));
+    p->fst = x;
+    p->snd = y;
+
+    return p;
+}
+
+elem pair_first(pair_t p){
+    return p->fst;
+}
+
+elem pair_second(pair_t p){
+    return p->snd;
+}
+
+pair_t pair_swapped(pair_t p){
+    pair_t q = malloc(sizeof(struct s_pair_t));
+    q->fst = p->snd;
+    q->snd = p->fst;
+
+    return q;
+}
+pair_t pair_destroy(pair_t p){
+    free(p);
+    return NULL;
+}
