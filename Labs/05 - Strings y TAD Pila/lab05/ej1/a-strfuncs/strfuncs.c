de294fe23b01873c2251ab25cf530abfeeaf4731
Lucas <lucas.sabino@mi.unc.edu.ar>
2025-01-26 13:24:02 -0300

    Ejercicio 1: Manejo de strings

--- Labs/05 - Strings y TAD Pila/lab05/ej1/a-strfuncs/strfuncs.c
+++ Labs/05 - Strings y TAD Pila/lab05/ej1/a-strfuncs/strfuncs.c
@@ -13,7 +13,43 @@ size_t string_length(const char *str){
 
 // Devuelve una nueva cadena en memoria dinamica que se obtiene tomando los caracteres
 // de str que son distintos del caracter c
-char *string_filter(const char *str, char c);
+char *string_filter(const char *str, char c){
+    unsigned int i = 0, j = 0, strlen = string_length(str), filterlen = 0;
+    // Calculo la longitud del stringg ya filtrado
+    while (i < strlen){
+        if (str[i] != c){
+            filterlen++;
+        }
+        i++;
+    }
 
-// Indicia si str es simetrico 
-bool string_is_symmetric(const char *str);
+    char * filtered = NULL;
+    filtered = malloc(sizeof(char) * (filterlen + 1));
+    if (filtered == NULL){
+        return NULL;
+    }
+
+    filtered[filterlen] = '\0';
+
+    i = 0;
+    while(i < strlen){
+        if(str[i] != c){
+            filtered[j] = str[i];
+            j++;
+        }
+        i++;
+    }
+    
+    return filtered;
+}
+
+bool string_is_symmetric(const char *str){
+    unsigned int i = 0;
+    while(i <= string_length(str)/2){
+        if(str[i] != str[string_length(str) -1 -i]){
+            return false;    
+        }
+        i++;
+    }
+    return true;
+}
