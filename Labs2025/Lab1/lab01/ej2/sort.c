65f8224c94c3ab3a1a32b375f12ff35b293621c8
Lucas <lucas.sabino@mi.unc.edu.ar>
2025-03-25 18:59:45 -0300

    ej2: Quick sort

--- Labs2025/Lab1/lab01/ej2/sort.c
+++ Labs2025/Lab1/lab01/ej2/sort.c
@@ -8,12 +8,12 @@
 
 
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
- /* needs implementation */
-
-    /* no implementes partition, ya está implementado en sort_helpers.o
-       (no se puede leer, pero en sort_helpers.h vas a encontrar información
-        para saber cómo usarlo)
-    */
+    unsigned int piv;
+    if (der > izq){
+        partition(a, izq, der);
+        quick_sort_rec(a, izq, piv-1);
+        quick_sort_rec(a, piv + 1, der);
+    }
 }
 
 void quick_sort(int a[], unsigned int length) {
