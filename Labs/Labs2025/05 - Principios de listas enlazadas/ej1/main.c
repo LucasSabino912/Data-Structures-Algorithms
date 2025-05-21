4d84b9dc861b16aa4f2ce114ff7dcdabe52e3187
Lucas <lucas.sabino@mi.unc.edu.ar>
2025-05-21 09:53:03 -0300

    05 - Principrios de listas enlazadas

--- Labs/Labs2025/05 - Principios de listas enlazadas/ej1/main.c
+++ Labs/Labs2025/05 - Principios de listas enlazadas/ej1/main.c
@@ -12,43 +12,30 @@ typedef node *List;
  * @brief Construye y devuelve una lista de ejemplo de 3 elementos
  */
 List setup_example() {
-    node *n1 = malloc(sizeof(node));
-    node *n2 = malloc(sizeof(node));
-    node *n3 = malloc(sizeof(node));
+    // Crea los nodos dinamicamente
+    node* n1 = malloc(sizeof(node));
+    node* n2 = malloc(sizeof(node));
+    node* n3 = malloc(sizeof(node));
 
-    if (n1 == NULL || n2 == NULL || n3 == NULL){
+    // Verificar que la memoria se haya asignado correctamente
+    if(n1 == NULL || n2 == NULL || n3 == NULL){
         free(n1);
         free(n2);
         free(n3);
         return NULL;
     }
 
+    // Asigno valores a los nodos
     n1->data = 10;
     n2->data = 20;
     n3->data = 30;
 
+    // Enlazar los nodos
     n1->next = n2;
     n2->next = n3;
     n3->next = NULL;
 
     return n1;
-}
-
-/**
- * @brief Elimina el primer elemento de la lista
- *
- * Precondicion: la lista xs no debe ser vacía
- */
-List tail_example(List xs) {
-    if(xs == NULL){
-        exit(EXIT_SUCCESS);
-    }
-
-    node *new_node = xs;
-    xs = xs->next;
-    free(new_node);
-
-    return xs;
 }
 
 void show_list(List xs) {
@@ -64,14 +51,6 @@ int main(void) {
     List my_list;
 
     my_list = setup_example();
-
-    printf("Lista antes del tail: ");
-
-    show_list(my_list);
-
-    my_list = tail_example(my_list);
-
-    printf("Lista después del tail: ");
 
     show_list(my_list);
 
--- Labs/Labs2025/05 - Principios de listas enlazadas/ej2/main.c
+++ Labs/Labs2025/05 - Principios de listas enlazadas/ej2/main.c
@@ -1,20 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node {
+typedef struct node {
     int data;
-    struct Node* next;
-} Node;
+    struct node* next;
+} node;
 
-typedef Node *List;
+typedef node *List;
 
 /**
  * @brief Construye y devuelve una lista de ejemplo de 3 elementos
  */
 List setup_example() {
-    //
-    // COMPLETAR ACÁ!
-    //
+    node *n1 = malloc(sizeof(node));
+    node *n2 = malloc(sizeof(node));
+    node *n3 = malloc(sizeof(node));
+
+    if (n1 == NULL || n2 == NULL || n3 == NULL){
+        free(n1);
+        free(n2);
+        free(n3);
+        return NULL;
+    }
+
+    n1->data = 10;
+    n2->data = 20;
+    n3->data = 30;
+
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = NULL;
+
+    return n1;
+}
+
+/**
+ * @brief Agrega un elemento de ejemplo al final de la lista
+ *
+ * Precondicion: la lista xs no debe ser vacía
+ */
+void append_example(List xs) {
+    if(xs == NULL){
+        exit(EXIT_SUCCESS);
+    }
+
+    node *napp = malloc(sizeof(node));
+    napp->data = 912;
+
+    if(napp == NULL){
+        exit(EXIT_SUCCESS);
+    }
+
+    while (xs->next != NULL){
+        xs = xs->next;
+    }
+
+    xs->next = napp;
 }
 
 void show_list(List xs) {
@@ -30,6 +71,14 @@ int main(void) {
     List my_list;
 
     my_list = setup_example();
+
+    printf("Lista antes del append: ");
+
+    show_list(my_list);
+
+    append_example(my_list);
+
+    printf("Lista después del append: ");
 
     show_list(my_list);
 
